89e1a580e9405c727d4e49ee212230f3175b5d55
mansibavliya <109659593+mansibavliya@users.noreply.github.com>
2022-11-21 09:33:01 +0530

    portfolio

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzW6b5PD9WlZ1D8uGNWAvq0yiW2HOYaWZO-GqKlm-h85Gk7fGYmIj2WbFH_U-Sm1ODMfw/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
